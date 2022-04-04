3880763574008281a2b3529fc2be5bd49dff5cb4
aknotj <100099666+aknotj@users.noreply.github.com>
2022-04-04 13:00:26 +0000

    [Add] User edit update機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
   end
@@ -10,18 +10,26 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(@user.id)
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
-  
+
 private
 
 def user_params
   params.require(:user).permit(:name, :introduction, :image)
 end
-  
+
 end
