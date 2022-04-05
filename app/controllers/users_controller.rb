e71a2b5b1b7abb625441adc002e0631f85a81c4d
aknotj <100099666+aknotj@users.noreply.github.com>
2022-04-05 01:17:22 +0000

    [Fix] Book Editページ Create Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "Your profile was successfully updated."
       redirect_to user_path(@user.id)
     else
       render :edit
